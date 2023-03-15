a797ac8155f61704693225a6b9894254762a8b34
Dimitri96 <dimitri96>
2023-03-15 22:05:25 +0100

    my changes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 short int max;
 
@@ -43,13 +44,13 @@ void findMaximum(short int arr1[], short int arr2[], int N)
 	printf("\n");
 }
 
+
 int main() {
 	// 1. first read in the file from the data folder
 	//1. define pointers to read in data
 	short int *data_im;
 	short int *data_re;
-	float *ref_data_im;
-	float *ref_data_re;
+
 	//short int **data_im_start = &data_im;
 	//short int **data_re_start = &data_re;
 	//2. allocate memory for the data to read in
@@ -61,18 +62,42 @@ int main() {
 	short int size = fsize / 4;
 	data_im = (short int*) malloc(size*sizeof(short int));
 	data_re = (short int*) malloc(size*sizeof(short int));
-	float re_Array[size];
-	float im_Array[size];
+	double re_Array[size];
+	double im_Array[size];
+
+	//get size of refWaveform
+	FILE *fp;
+    int count = 0; // Line counter (result)
+    char c; // To store a character read from file
+	fp = fopen("refWaveform", "r");
+	for (c = getc(fp); c != EOF; c = getc(fp))
+	{
+        if (c == '\n') // Increment count if this character is newline
+            count = count + 1;
+	}
+    // Close the file
+    fclose(fp);
 	
-	FILE *fp = fopen("refWaveform", "r");
-	fseek(fp, 0, SEEK_END);
-	long fpsize = ftell(fp);
-	fseek(fp, 0, SEEK_SET); 
+	//refWaveform Arrays
+	double re_Array_ref[count];
+	double im_Array_ref[count];
+	fp = fopen("refWaveform", "r");
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int current_position = 0;
+	char *token;
+	while((read = getline(&line, &len, fp)) !=-1)
+	{
+		token = strtok(line, " + "); //split on " + "
+		re_Array_ref[current_position] = atof(token); //atof reads in current token value into Array and parses as double
+		token = strtok(NULL, " + "); //split on " + "
+		token[strlen(token)-1] = '\0'; //remove the j 
+		im_Array_ref[current_position] = atof(token);
+		
 
-	short int sz = fpsize / 4;
-	ref_data_im = (float*) malloc(sz*sizeof(float));
-	ref_data_re = (float*) malloc(sz*sizeof(float));
-
+	}
+	
 
 	//go in steps of 2 bytes and split the data into real and imaginery numbers, each with a size of 2 bytes
 	int i;
@@ -85,44 +110,16 @@ int main() {
 		
 	}
 
-	for (i=0; i<sz; i++)
-	{
-		fread(&ref_data_re[i], 2, 1, fp);
-		fseek(fp, 2, SEEK_CUR);
-		fread(&ref_data_im[i], 2, 1, fp);
-		fseek(fp, 2, SEEK_CUR);
-		
-	}
-
-
 	findMaximum(data_re, data_im, size);
 
 	for(int i=0; i<size; i++)
 	{
-		re_Array[i] = ((float)data_re[i])/max;
-		im_Array[i] = ((float)data_im[i])/max;
+		re_Array[i] = ((double)data_re[i])/max;
+		im_Array[i] = ((double)data_im[i])/max;
 	}
 
+//(ac -bd) + i(ad +bc) multiplying two complex numbers
 
-
-
-
-
-	
-	i = 0;
-	for (i=0; i<10; i++)
-	{
-		printf("ref_data_im %f\n", ref_data_im[i]);
-		printf("ref_data_re %f\n", ref_data_re[i]);
-
-	}
-	fclose(f);
-
-	/*for (int i=0; i<size; i++)
-	{
-		re_Array[i] = re_Array[i] * exp();
-		re_Array[i] = re_Array[i] * exp();
-	}*/
 
 
 	return 0;
